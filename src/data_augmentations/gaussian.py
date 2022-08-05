81be958f35dbcc9191a2994ec481e2689ad77945
Rafay Khan <rafayak16@gmail.com>
2022-08-05 22:20:32 +0500

    rename iterations var with n_augmentations, remove redundant copy, add line break

--- src/data_augmentations/gaussian.py
+++ src/data_augmentations/gaussian.py
@@ -2,7 +2,7 @@ from typing import Tuple
 import numpy as np
 
 
-def augment_data(X: np.ndarray, Y: np.ndarray, noise: float = 0.05, iterations: int = 1) -> Tuple[np.ndarray, np.ndarray]:
+def augment_data(X: np.ndarray, Y: np.ndarray, noise: float = 0.05, n_augmentations: int = 1) -> Tuple[np.ndarray, np.ndarray]:
     """
     Augment data function takes in a SkLearn point dataset (moons, circles, blobs etc.) and creates a new dataset.
     The new dataset is a combination of the original dataset + the original dataset distorted by random Gaussian noise.
@@ -12,7 +12,7 @@ def augment_data(X: np.ndarray, Y: np.ndarray, noise: float = 0.05, iterations: 
         Y (np.ndarray): a numpy array with integer values denoting class labels for all data points in X
         noise (float): the amount of Gaussian noise to add to each sample point in X. Higher the noise the more
                        distorted the new points. Default 0.05
-        iterations (int): the number of time to run the original dataset through the augmentation process to create
+        n_augmentations (int): the number of time to run the original dataset through the augmentation process to create
                           new data. Default 1
 
     Returns:
@@ -25,11 +25,10 @@ def augment_data(X: np.ndarray, Y: np.ndarray, noise: float = 0.05, iterations: 
     # seed value can be set in the experiment configs globally
     rng = np.random.default_rng()
 
-    original_X, original_Y = X.copy(), Y.copy()
-
     new_X, new_Y = X.copy(), Y.copy()
-    for _ in range(iterations):
-        augmented_X, augmented_Y = original_X.copy(), original_Y.copy()
+
+    for _ in range(n_augmentations):
+        augmented_X, augmented_Y = X.copy(), Y.copy()
         augmented_X += rng.normal(scale=noise, size=augmented_X.shape)
         new_X, new_Y = np.vstack((new_X, augmented_X)), np.vstack((new_Y, augmented_Y))
 

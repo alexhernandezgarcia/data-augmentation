73b8a95d7e8e046133bf09613563cec5e0a69862
Rafay Khan <rafayak16@gmail.com>
2022-08-07 19:41:09 +0500

    fix variable name change

--- scripts/moons_run.sh
+++ scripts/moons_run.sh
@@ -3,8 +3,8 @@
 
 # run baseline experiments for various samples without any augmentation
 python src/train.py -m experiment=moons_experiment.yaml logger.wandb.project='moons_experiment' \
-datamodule.dataset.n_samples=20,50,100,250,500,1000,5000,10000 datamodule.data_aug.iterations=0
+datamodule.dataset.n_samples=20,50,100,250,500,1000,5000,10000 datamodule.data_aug.n_augmentations=0
 
-# run same experiment as above now with 5 augmentation n_augmentations
+# run same experiment as above now with each original data point augmented 5 times
 python src/train.py -m experiment=moons_experiment.yaml logger.wandb.project='moons_experiment' \
-datamodule.dataset.n_samples=20,50,100,250,500,1000,5000,10000 datamodule.data_aug.iterations=5
+datamodule.dataset.n_samples=20,50,100,250,500,1000,5000,10000 datamodule.data_aug.n_augmentations=5

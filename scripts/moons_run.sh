d9995b7d3e4a8eed472aaa2e417b55709d35dcd7
Rafay Khan <rafayak16@gmail.com>
2022-08-17 16:32:31 +0500

    update bash script for multirun moons experiment

--- scripts/moons_run.sh
+++ scripts/moons_run.sh
@@ -2,9 +2,21 @@
 # Run from root folder with: bash scripts/moons_run.sh
 
 # run baseline experiments for various samples without any augmentation
-python src/train.py -m experiment=moons_experiment.yaml logger.wandb.project='moons_experiment' \
-datamodule.train_dataset.n_samples=20,50,100,250,500,1000,5000,10000 datamodule.data_aug.n_augmentations=0
+python src/train.py -m experiment=moons_experiment.yaml trainer=gpu \
+logger.wandb.project='moons_experiment_3' \
+model.layers="[2,20,30,20,1]" \
+model.lr=0.1 \
+trainer.min_epochs=100 \
+trainer.max_epochs=100 \
+datamodule.train_dataset.n_samples=20,50,100,250,500,1000,5000,10000 \
+datamodule.data_aug.n_augmentations=0
 
-# run same experiment as above now with each original data point augmented 5 times
-python src/train.py -m experiment=moons_experiment.yaml logger.wandb.project='moons_experiment' \
-datamodule.train_dataset.n_samples=20,50,100,250,500,1000,5000,10000 datamodule.data_aug.n_augmentations=5
+# run same experiment as above with less datapoints, but each original data point augmented [1,9] times
+python src/train.py -m experiment=moons_experiment.yaml trainer=gpu \
+logger.wandb.project='moons_experiment_3' \
+model.layers="[2,20,30,20,1]" \
+model.lr=0.1 \
+trainer.min_epochs=100 \
+trainer.max_epochs=100 \
+datamodule.train_dataset.n_samples=5,10,20,50,100,250 \
+datamodule.data_aug.n_augmentations="range(1, 10)"

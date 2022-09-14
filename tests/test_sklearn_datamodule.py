9216e2222f5c622e0c5456d204d8ccaf5d5f7f2e
Rafay Khan <rafayak16@gmail.com>
2022-09-14 19:01:30 +0500

    fixes by pre-commit

--- tests/test_sklearn_datamodule.py
+++ tests/test_sklearn_datamodule.py
@@ -1,17 +1,18 @@
 from pathlib import Path
 
 import hydra.utils
+import numpy as np
 import pytest
 import torch
+from hydra import compose, initialize
 from sklearn import datasets
-import numpy as np
+
 from src.datamodules.sklearn_datamodule import SklearnDataModule
-from hydra import initialize, compose
+
 
 def find_intersecting_rows(arr1: np.ndarray, arr2: np.ndarray):
     nrows, ncols = arr1.shape
-    dtype = {'names': ['f{}'.format(i) for i in range(ncols)],
-             'formats': ncols * [arr1.dtype]}
+    dtype = {"names": [f"f{i}" for i in range(ncols)], "formats": ncols * [arr1.dtype]}
 
     new_arr = np.intersect1d(arr1.view(dtype), arr2.view(dtype))
 
@@ -21,12 +22,16 @@ def find_intersecting_rows(arr1: np.ndarray, arr2: np.ndarray):
     return new_arr
 
 
-@pytest.mark.parametrize("train_dataset, val_dataset, test_dataset",
-                         [(datasets.make_moons(n_samples=20, shuffle=True, noise=0.0005, random_state=1234),
-                           datasets.make_moons(n_samples=1000, shuffle=True, noise=0.0005, random_state=1111),
-                           datasets.make_moons(n_samples=1000, shuffle=True, noise=0.0005, random_state=2222))
-                          ]
-                         )
+@pytest.mark.parametrize(
+    "train_dataset, val_dataset, test_dataset",
+    [
+        (
+            datasets.make_moons(n_samples=20, shuffle=True, noise=0.0005, random_state=1234),
+            datasets.make_moons(n_samples=1000, shuffle=True, noise=0.0005, random_state=1111),
+            datasets.make_moons(n_samples=1000, shuffle=True, noise=0.0005, random_state=2222),
+        )
+    ],
+)
 def test_sklearn_datamodule(train_dataset, val_dataset, test_dataset):
     x_train, y_train = train_dataset
     x_val, y_val = val_dataset
@@ -47,9 +52,15 @@ def test_sklearn_datamodule(train_dataset, val_dataset, test_dataset):
         dm = hydra.utils.instantiate(cfg)
 
         assert dm
-        combined_train = np.hstack((dm.train_dataset.X, np.reshape(dm.train_dataset.Y, (len(dm.train_dataset.Y), 1))))
-        combined_val = np.hstack((dm.val_dataset.X, np.reshape(dm.val_dataset.Y, (len(dm.val_dataset.Y), 1))))
-        combined_test = np.hstack((dm.test_dataset.X, np.reshape(dm.test_dataset.Y, (len(dm.test_dataset.Y), 1))))
+        combined_train = np.hstack(
+            (dm.train_dataset.X, np.reshape(dm.train_dataset.Y, (len(dm.train_dataset.Y), 1)))
+        )
+        combined_val = np.hstack(
+            (dm.val_dataset.X, np.reshape(dm.val_dataset.Y, (len(dm.val_dataset.Y), 1)))
+        )
+        combined_test = np.hstack(
+            (dm.test_dataset.X, np.reshape(dm.test_dataset.Y, (len(dm.test_dataset.Y), 1)))
+        )
         assert len(find_intersecting_rows(combined_train, combined_val)) == 0
         assert len(find_intersecting_rows(combined_train, combined_test)) == 0
         assert len(find_intersecting_rows(combined_val, combined_test)) == 0

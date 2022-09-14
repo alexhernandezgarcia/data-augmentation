a66753cfe8944797e70b2e46c18d03a36c9f0008
RafayAK <rafayak16@gmail.com>
2022-09-14 19:11:30 +0500

    Add test cases (#12)
    * force test_experiments function to use default, non-wandb callbacks only
    
    * add github workflows
    
    * add pre-commit hooks for linting
    
    * fix testing github workflow
    
    * update test.yml to use pipenv github workflow
    
    * fix 3.9 version typo
    
    * remove 3.9 and macos code coverage
    
    * fix pipenv setup
    
    * revert setup
    
    * ignore code-coverage for now
    
    * ignore wandb test for now
    
    * changes by pre-commit hooks
    
    * exclude running bandit on callbacks that require git access
    
    * add .env.example file for setting local env variables
    
    * add sklearn testcases
    
    * fixes by pre-commit

--- src/datamodules/sklearn_datamodule.py
+++ src/datamodules/sklearn_datamodule.py
@@ -1,20 +1,24 @@
-from typing import Tuple, Callable
+from typing import Callable, Tuple
+
 import numpy as np
 from pl_bolts.datamodules import SklearnDataModule
 from pytorch_lightning import LightningDataModule
-from src.utils.pylogger import get_pylogger
 from sklearn.model_selection import train_test_split
+
+from src.utils.pylogger import get_pylogger
 
 log = get_pylogger(__name__)
 
 
-def create_sklearn_datamodule(train_dataset: Tuple[np.ndarray, np.ndarray],
-                              val_dataset: Tuple[np.ndarray, np.ndarray],
-                              test_dataset: Tuple[np.ndarray, np.ndarray],
-                              data_aug: Callable = None, *args: object,
-                              **kwargs: object) -> LightningDataModule:
-    """
-    Helper function to create a LightningDataModule for Sklearn datasets.
+def create_sklearn_datamodule(
+    train_dataset: Tuple[np.ndarray, np.ndarray],
+    val_dataset: Tuple[np.ndarray, np.ndarray],
+    test_dataset: Tuple[np.ndarray, np.ndarray],
+    data_aug: Callable = None,
+    *args: object,
+    **kwargs: object,
+) -> LightningDataModule:
+    """Helper function to create a LightningDataModule for Sklearn datasets.
 
     # Parameters that can be passed as args and kwargs:
     # https://pytorch-lightning-bolts.readthedocs.io/en/latest/datamodules_sklearn.html#sklearn-datamodule-class
@@ -22,7 +26,7 @@ def create_sklearn_datamodule(train_dataset: Tuple[np.ndarray, np.ndarray],
         train_dataset (Tuple[np.ndarray, np.ndarray]): a Sklearn dataset, can be instantiated using Hydra
         val_dataset (Tuple[np.ndarray, np.ndarray]): a fixed Sklearn dataset to be used as validation dataset,
                                                      can be initialized through hydra
-        test_dataset (Tuple[np.ndarray, np.ndarray]): a fixed Sklearn dataset to be used as test dataset, 
+        test_dataset (Tuple[np.ndarray, np.ndarray]): a fixed Sklearn dataset to be used as test dataset,
                                                       can be initialized through hydra
         data_aug (Callable): a callable function/class/object that takes in the X (sample points) and Y (labels) and
                              return augmented dataset new_X and new_Y
@@ -31,10 +35,11 @@ def create_sklearn_datamodule(train_dataset: Tuple[np.ndarray, np.ndarray],
 
     Returns:
         datamodule (LightningDataModule): a PytorchLightening Datamodule
-
     """
     X_train, y_train = train_dataset
-    y_train = np.reshape(y_train, (len(y_train), 1))  # reshape to be (len(y_train), 1) vector instead of a flat array
+    y_train = np.reshape(
+        y_train, (len(y_train), 1)
+    )  # reshape to be (len(y_train), 1) vector instead of a flat array
 
     # setup validation set
     X_val, y_val = val_dataset
@@ -47,20 +52,23 @@ def create_sklearn_datamodule(train_dataset: Tuple[np.ndarray, np.ndarray],
     # augment data if a data_aug callable function is provided
     if data_aug:
         log.info(f"Data augmentation function provided {data_aug.__repr__()}")
-        log.info(f"Total dataset size before augmentation: {len(y_train) + len(y_val) + len(y_test)}")
+        log.info(
+            f"Total dataset size before augmentation: {len(y_train) + len(y_val) + len(y_test)}"
+        )
         log.info(f"Training set size before augmentation: {len(y_train)}")
         X_train, y_train = data_aug(X_train, y_train)
         log.info(f"Training set size after augmentation: {len(y_train)}")
-        log.info(f"Total dataset size after augmentation: {len(y_train) + len(y_val) + len(y_test)}")
+        log.info(
+            f"Total dataset size after augmentation: {len(y_train) + len(y_val) + len(y_test)}"
+        )
 
     # create SklearnDataModule, which is just a LightningDataModule
-    datamodule: SklearnDataModule = SklearnDataModule(X_train, y_train,
-                                                      x_val=X_val, y_val=y_val,
-                                                      x_test=X_test, y_test=y_test,
-                                                      *args, *kwargs)
+    datamodule: SklearnDataModule = SklearnDataModule(
+        X_train, y_train, x_val=X_val, y_val=y_val, x_test=X_test, y_test=y_test, *args, *kwargs
+    )
 
     # log info about the dataset.
-    log.info(f"SklearnDataModule stats: ")
+    log.info("SklearnDataModule stats:")
     log.info(f"\tNumber of Training examples: {len(datamodule.train_dataset)}")
     log.info(f"\tNumber of Validation examples: {len(datamodule.val_dataset)}")
     log.info(f"\tNumber of Testing examples: {len(datamodule.test_dataset)}")

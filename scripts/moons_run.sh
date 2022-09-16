b5296fd1da1da26ece496dda3564ff1cba502fb1
Rafay Khan <rafayak16@gmail.com>
2022-09-17 01:15:52 +0500

    update bash file according to new base samples range

--- scripts/moons_run.sh
+++ scripts/moons_run.sh
@@ -3,20 +3,22 @@
 
 # run baseline experiments for various samples without any augmentation
 python src/train.py -m experiment=moons_experiment.yaml trainer=gpu \
-logger.wandb.project='moons_experiment_5' \
++logger.wandb.entity=alex_data_augmentation \
+logger.wandb.project=moons_experiment_5 \
 model.layers="[2,20,30,20,1]" \
 model.lr=0.1 \
 trainer.min_epochs=100 \
 trainer.max_epochs=100 \
-datamodule.train_dataset.n_samples=20,50,100,250,500,1000,5000,10000 \
+datamodule.train_dataset.n_samples=6,10,15,20,25,30,35,40,45,50,100 \
 datamodule.data_aug.n_augmentations=0
 
 # run same experiment as above with less datapoints, but each original data point augmented [1,9] times
 python src/train.py -m experiment=moons_experiment.yaml trainer=gpu \
-logger.wandb.project='moons_experiment_5' \
++logger.wandb.entity=alex_data_augmentation \
+logger.wandb.project=moons_experiment_5 \
 model.layers="[2,20,30,20,1]" \
 model.lr=0.1 \
 trainer.min_epochs=100 \
 trainer.max_epochs=100 \
-datamodule.train_dataset.n_samples=5,10,20,50,100,250 \
-datamodule.data_aug.n_augmentations="range(1, 10)"
+datamodule.train_dataset.n_samples=6,8,10,12,16 \
+datamodule.data_aug.n_augmentations=4,6,9,12,15

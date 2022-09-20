b9a4acb901ac2b63d4538accb22d3ff18876f4b8
Rafay Khan <rafayak16@gmail.com>
2022-09-20 23:20:29 +0500

    hack base_samples into experiments where it's not there

--- src/callbacks/wandb_callbacks.py
+++ src/callbacks/wandb_callbacks.py
@@ -660,3 +660,8 @@ class AddToConfigEffectiveTrainSize(Callback):
         valX, valY = train_data.X, train_data.Y
 
         experiment.config.update({"effective_training_size": len(valY)})
+
+        # hack to add base samples to the config
+        if 'datamodule/train_val_test_split' in logger.experiment.config._items:
+            base_n_samples = logger.experiment.config._items['datamodule/train_val_test_split'][0]
+            experiment.config.update({'datamodule/train_dataset/n_samples': base_n_samples})

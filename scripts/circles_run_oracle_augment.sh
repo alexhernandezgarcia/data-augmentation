2548b4a8c4df03795ce14282db7dbc4a5ab04711
Rafay Khan <rafayak16@gmail.com>
2022-09-24 12:03:35 +0500

    update run files to use environment variables

--- scripts/circles_run_oracle_augment.sh
+++ scripts/circles_run_oracle_augment.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 # Run from root folder with: bash scripts/moons_run_oracle_augment.sh
 
+export EXPERIMENT_FILE="circles_experiment_oracle.yaml"
+export PROJECT_NAME="circles_experiment_data_bank_sampling_oracle_aug"
+export LEARNING_RATE=0.3
+export EPOCHS=135
+
 # run baseline experiments for various samples without any augmentation
-python src/train.py -m experiment=circles_experiment_oracle.yaml trainer=gpu \
+python src/train.py -m experiment=$EXPERIMENT_FILE trainer=gpu \
 +logger.wandb.entity=alex_data_augmentation \
-logger.wandb.project=circles_experiment_oracle_augment \
+logger.wandb.project=$PROJECT_NAME \
 model.layers="[2,20,30,20,1]" \
-model.lr=0.1 \
-trainer.min_epochs=100 \
-trainer.max_epochs=100 \
+model.lr=$LEARNING_RATE \
+trainer.min_epochs=$EPOCHS \
+trainer.max_epochs=$EPOCHS \
 datamodule.train_val_test_split.0=6,10,15,20,25,30,35,40,45,50,100 \
 datamodule.data_aug.n_augmentations=0
 
 # run same experiment as above with less datapoints, but each original data point augmented [1,9] times
-python src/train.py -m experiment=circles_experiment_oracle.yaml trainer=gpu \
+python src/train.py -m experiment=$EXPERIMENT_FILE trainer=gpu \
 +logger.wandb.entity=alex_data_augmentation \
-logger.wandb.project=circles_experiment_oracle_augment \
+logger.wandb.project=$PROJECT_NAME \
 model.layers="[2,20,30,20,1]" \
-model.lr=0.1 \
-trainer.min_epochs=100 \
-trainer.max_epochs=100 \
+model.lr=$LEARNING_RATE \
+trainer.min_epochs=$EPOCHS \
+trainer.max_epochs=$EPOCHS \
 datamodule.train_val_test_split.0=6,8,10,12,16 \
 datamodule.data_aug.n_augmentations=4,6,9,12,15

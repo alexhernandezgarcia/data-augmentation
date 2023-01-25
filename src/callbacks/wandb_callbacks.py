19a4cd833a9a3ed44917b0a399709ce67a2ae0da
Rafay Khan <rafayak16@gmail.com>
2023-01-25 23:03:52 +0500

    remove depreciated LoggerCollection for now

--- src/callbacks/wandb_callbacks.py
+++ src/callbacks/wandb_callbacks.py
@@ -10,7 +10,7 @@ import seaborn as sn
 import torch
 import wandb
 from pytorch_lightning import Callback, Trainer
-from pytorch_lightning.loggers import LoggerCollection, WandbLogger
+from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.utilities import rank_zero_only
 from sklearn import metrics
 from sklearn.metrics import f1_score, precision_score, recall_score
@@ -31,10 +31,10 @@ def get_wandb_logger(trainer: Trainer) -> WandbLogger:
     if isinstance(trainer.logger, WandbLogger):
         return trainer.logger
 
-    if isinstance(trainer.logger, LoggerCollection):
-        for logger in trainer.logger:
-            if isinstance(logger, WandbLogger):
-                return logger
+    # if isinstance(trainer.logger, LoggerCollection):
+    #     for logger in trainer.logger:
+    #         if isinstance(logger, WandbLogger):
+    #             return logger
 
     raise Exception(
         "You are using wandb related callback, but WandbLogger was not found for some reason..."
